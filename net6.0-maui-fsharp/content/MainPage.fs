bca6bf67d9be389226ab05d694d38793241cfd8e
TimLariviere <timothe.lariviere@outlook.com>
2022-01-27 11:39:16 +0100

    Experimenting with MSBuild fix for MAUI on iOS

--- net6.0-maui-fsharp/content/MainPage.fs
+++ net6.0-maui-fsharp/content/MainPage.fs
@@ -5,4 +5,9 @@ open Microsoft.Maui.Controls
 type MainPage() as this =
     inherit ContentPage()
     
-    do this.Content <- Label(Text = "Hello from MAUI, .NET 6.0 and F# 6.0!")
+    do this.Content <-
+        Label(
+            Text = "Hello from MAUI, .NET 6.0 and F# 6.0!",
+            VerticalOptions = LayoutOptions.Center,
+            HorizontalOptions = LayoutOptions.Center
+        )
